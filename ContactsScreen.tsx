43466b656b100b9ce85522200596c58628b64950
David Ernst <git@dsernst.com>
2022-03-07 01:48:54 -0800

    Replace Contacts' List w/ optimized SectionList

--- ContactsScreen.tsx
+++ ContactsScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
+import { View, Text, TouchableOpacity, SectionList } from 'react-native'
 import { OurModal } from './Modal'
 import { ScreenProps } from './App'
 
@@ -13,9 +13,20 @@ export function ContactsScreen({
     false
   )
 
-  function handlePress(id: string): void {
-    setSelectedContactId(id)
-  }
+  // Group contacts by first letter of last name
+  const contactsGroupedByFirstLetter = contacts.reduce((memo, contact) => {
+    const name = contact.lastName || contact.firstName || ' '
+    const firstLetter = name[0]
+    if (!memo[firstLetter]) {
+      memo[firstLetter] = []
+    }
+    memo[firstLetter].push(contact)
+    return memo
+  }, {} as Record<string, typeof contacts>)
+  const data = Object.keys(contactsGroupedByFirstLetter).map((letter) => ({
+    letter,
+    data: contactsGroupedByFirstLetter[letter],
+  }))
 
   return (
     <View>
@@ -38,7 +49,9 @@ export function ContactsScreen({
           paddingVertical: 5,
         }}
       >
-        <Text style={{ fontSize: 15, fontWeight: '800' }}>Contacts</Text>
+        <Text style={{ fontSize: 15, fontWeight: '800', opacity: 0.6 }}>
+          Contacts
+        </Text>
         <TouchableOpacity
           style={{
             borderWidth: 1,
@@ -54,27 +67,44 @@ export function ContactsScreen({
         </TouchableOpacity>
       </View>
 
-      <ScrollView
-        style={{ width: '100%', paddingHorizontal: 10, paddingVertical: 10 }}
-      >
-        {contacts.map(({ firstName, lastName, id }) => (
+      <SectionList
+        sections={data}
+        keyExtractor={({ id }) => id}
+        renderItem={({ item: { id, firstName, lastName } }) => (
           <TouchableOpacity
             style={{
               marginVertical: 2,
               flexDirection: 'row',
               alignItems: 'center',
               justifyContent: 'space-between',
+              padding: 12,
+              borderBottomWidth: 1,
+              borderBottomColor: '#0002',
             }}
             key={id}
-            onPress={() => handlePress(id)}
+            onPress={() => setSelectedContactId(id)}
           >
             <Text style={{ fontSize: 18 }}>
-              {firstName} {lastName}
+              {firstName} <Text style={{ fontWeight: '600' }}>{lastName}</Text>
             </Text>
             <Text style={{ opacity: 0.4 }}>{frequencies[id]}</Text>
           </TouchableOpacity>
-        ))}
-      </ScrollView>
+        )}
+        renderSectionHeader={({ section: { letter } }) => (
+          <Text
+            style={{
+              backgroundColor: '#f3f3f3',
+              color: '#0006',
+              fontSize: 16,
+              fontWeight: '600',
+              paddingHorizontal: 10,
+              paddingVertical: 2,
+            }}
+          >
+            {letter}
+          </Text>
+        )}
+      />
     </View>
   )
 }
