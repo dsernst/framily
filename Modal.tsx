cc2d7d015bf2ff76f408a2237462ea4fa5a03da6
David Ernst <git@dsernst.com>
2022-03-06 19:48:16 -0800

    Improve look of Modal

--- Modal.tsx
+++ Modal.tsx
@@ -1,5 +1,5 @@
 import React, { Dispatch, SetStateAction } from "react"
-import { Modal, StyleSheet, Text, Pressable, View, Picker } from "react-native"
+import { Modal, Text, View, Picker, TouchableOpacity } from "react-native"
 import { Frequencies } from "./App"
 
 export const OurModal = ({
@@ -23,12 +23,32 @@ export const OurModal = ({
         setSelectedContactId(false)
       }}
     >
-      <View style={styles.centeredView}>
-        <View style={styles.modalView}>
-          <Text style={styles.modalText}>Minimum time till catchup</Text>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "white",
+            borderRadius: 10,
+            padding: 35,
+            alignItems: "center",
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+          }}
+        >
+          <Text style={{ fontSize: 18 }}>Minimum time till catchup?</Text>
           <Picker
-            itemStyle={styles.pickerItem}
-            style={styles.picker}
+            itemStyle={{ color: "grey" }}
+            style={{ width: 300 }}
             selectedValue={frequencies[selectedContactId]}
             onValueChange={(itemValue) =>
               setFrequencies({
@@ -50,64 +70,29 @@ export const OurModal = ({
               <Picker.Item key={val} label={val} value={val} />
             ))}
           </Picker>
-          <Pressable
-            style={[styles.button, styles.buttonClose]}
+          <TouchableOpacity
+            style={{
+              borderRadius: 5,
+              paddingVertical: 10,
+              width: 280,
+              elevation: 2,
+              backgroundColor: "#2196F3",
+            }}
+            activeOpacity={0.7}
             onPress={() => setSelectedContactId(false)}
           >
-            <Text style={styles.textStyle}>Close Modal</Text>
-          </Pressable>
+            <Text
+              style={{
+                color: "white",
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              Back
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </Modal>
   )
 }
-
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-  },
-  picker: {
-    width: 300,
-  },
-  pickerItem: {
-    color: "grey",
-  },
-})
