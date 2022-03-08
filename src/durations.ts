6801a0c114df7cb66ed1f5cf46060f2297155fa1
David Ernst <git@dsernst.com>
2022-03-07 17:32:50 -0800

    Tweak durations

--- src/durations.ts
+++ src/durations.ts
@@ -1,12 +1,26 @@
-export const options = [undefined, 7, 14, 30, 90, 180, 365]
+const YEAR = 365
+const MONTH = 30
+const WEEK = 7
+export const options = [
+  undefined,
+  WEEK,
+  2 * WEEK,
+  MONTH,
+  2 * MONTH,
+  3 * MONTH,
+  6 * MONTH,
+  YEAR,
+  2 * YEAR,
+  5 * YEAR,
+]
 
 export const daysToString = (days?: number) => {
   if (!days) return 'Not set'
 
   const units: [number, string][] = [
-    [365, 'year'],
-    [30, 'month'],
-    [7, 'week'],
+    [YEAR, 'year'],
+    [MONTH, 'month'],
+    [WEEK, 'week'],
     [1, 'day'],
   ]
   for (const [duration, unitName] of units) {
