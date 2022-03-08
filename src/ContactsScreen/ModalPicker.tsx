374cc7cbd2089bb23a80b76ac7565ebd52435079
David Ernst <git@dsernst.com>
2022-03-07 17:03:22 -0800

    ModalPicker: Dismiss by tapping anywhere outside

--- src/ContactsScreen/ModalPicker.tsx
+++ src/ContactsScreen/ModalPicker.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Modal, Text, View, TouchableOpacity } from 'react-native'
+import { Modal, Text, View, TouchableWithoutFeedback } from 'react-native'
 import { Picker } from '@react-native-picker/picker'
 import { useStorage } from '../useStorage'
 import { Selected } from './ContactsScreen'
@@ -19,98 +19,76 @@ export const ModalPicker = ({
 
   if (!selected) return null
   return (
-    <Modal
-      transparent
-      visible={!!selected}
-      onRequestClose={() => {
-        setSelected(false)
-      }}
-    >
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: '#000c',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
+    <Modal transparent visible={!!selected}>
+      <TouchableWithoutFeedback onPress={() => setSelected(false)}>
         <View
           style={{
-            backgroundColor: 'white',
-            borderRadius: 10,
-            padding: 35,
+            flex: 1,
+            backgroundColor: '#000c',
+            justifyContent: 'center',
             alignItems: 'center',
-            shadowColor: '#000',
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 4,
           }}
         >
-          <Text
-            style={{
-              fontSize: 20,
-              marginBottom: 20,
-              fontWeight: '600',
-              opacity: 0.8,
-            }}
-          >
-            {selected.firstName} {selected.lastName}
-          </Text>
-
-          <Text style={{ fontSize: 14 }}>Minimum time till catchup?</Text>
-          <Picker
-            itemStyle={{ color: 'grey' }}
-            style={{ width: 300 }}
-            selectedValue={frequencies[selected.id]}
-            onValueChange={(itemValue) => {
-              update({
-                frequencies: {
-                  ...frequencies,
-                  [selected.id]:
-                    itemValue !== 'Not set' ? itemValue : undefined,
-                },
-              })
-              setSelected(false)
-            }}
-          >
-            {[
-              'Not set',
-              '1 week',
-              '2 weeks',
-              '1 month',
-              '3 months',
-              '6 months',
-              '1 year',
-            ].map((val) => (
-              <Picker.Item key={val} label={val} value={val} />
-            ))}
-          </Picker>
-          <TouchableOpacity
-            style={{
-              borderRadius: 5,
-              paddingVertical: 10,
-              width: 280,
-              elevation: 2,
-              backgroundColor: '#2196F3aa',
-            }}
-            activeOpacity={0.7}
-            onPress={() => setSelected(false)}
-          >
-            <Text
+          <TouchableWithoutFeedback onPress={() => {}}>
+            <View
               style={{
-                color: 'white',
-                fontWeight: 'bold',
-                textAlign: 'center',
+                backgroundColor: 'white',
+                borderRadius: 10,
+                padding: 35,
+                paddingBottom: 0,
+                alignItems: 'center',
+                shadowColor: '#000',
+                shadowOffset: {
+                  width: 0,
+                  height: 2,
+                },
+                shadowOpacity: 0.25,
+                shadowRadius: 4,
               }}
             >
-              Cancel
-            </Text>
-          </TouchableOpacity>
+              <Text
+                style={{
+                  fontSize: 20,
+                  marginBottom: 20,
+                  fontWeight: '600',
+                  opacity: 0.8,
+                }}
+              >
+                {selected.firstName} {selected.lastName}
+              </Text>
+
+              <Text style={{ fontSize: 14 }}>Minimum time till catchup?</Text>
+              <Picker
+                itemStyle={{ color: 'grey' }}
+                style={{ width: 300 }}
+                selectedValue={frequencies[selected.id]}
+                onValueChange={(itemValue) => {
+                  update({
+                    frequencies: {
+                      ...frequencies,
+                      [selected.id]:
+                        itemValue !== 'Not set' ? itemValue : undefined,
+                    },
+                  })
+                  setSelected(false)
+                }}
+              >
+                {[
+                  'Not set',
+                  '1 week',
+                  '2 weeks',
+                  '1 month',
+                  '3 months',
+                  '6 months',
+                  '1 year',
+                ].map((val) => (
+                  <Picker.Item key={val} label={val} value={val} />
+                ))}
+              </Picker>
+            </View>
+          </TouchableWithoutFeedback>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     </Modal>
   )
 }
